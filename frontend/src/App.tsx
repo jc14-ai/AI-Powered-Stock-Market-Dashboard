9ff09e0bb4b51c527478287e0a508e241a83203c
Jzy <jestalycastillo15@gmail.com>
2025-08-15 21:07:10 +0800

    Add: Navbar.tsx component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,9 @@
-function App() {
+import Navbar from "./components/Navbar.tsx"
 
+export default function App() {
   return (
     <>
-    <div className='flex-col justify-center border-2 m-0 bg-amber-400 h-80 w-72'></div>
-    {/* TODO: Put landing page here, or just directly the dashboard (not decided yet) */}
+      <Navbar />
     </>
   )
 }
-
-export default App
