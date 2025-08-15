9b16dd8dbc60e0062154b719e9735a95507d8db6
Jzy <jestalycastillo15@gmail.com>
2025-08-15 20:43:10 +0800

    Add: tailwindcss package

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,8 @@
-import './styles/App.module.css'
-
 function App() {
 
   return (
     <>
+    <div className='flex-col justify-center border-2 m-0 bg-amber-400 h-80 w-72'></div>
     {/* TODO: Put landing page here, or just directly the dashboard (not decided yet) */}
     </>
   )
