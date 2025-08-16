9c375be9d3d6f51d54b130b3baf387e4421903da
Jzy <jestalycastillo15@gmail.com>
2025-08-16 21:40:41 +0800

    Fix: theme toggle logo

--- frontend/src/features/NavBar.tsx
+++ frontend/src/features/NavBar.tsx
@@ -1,29 +1,41 @@
-import { useRef } from "react";
+import { useState } from "react";
 import darkMode from "../assets/dark_mode.png";
 import lightMode from "../assets/light_mode.png";
 import logo from "../assets/microsoft.png";
 
 export default function Navbar (){
     // TODO: Implement and fix theme toggle functionality
-    const theme = useRef<HTMLImageElement>(null);
+    // const themeRef = useRef<HTMLImageElement>(null);
 
-    const handleThemeMode = ():void => {
+    // const handleThemeMode = ():void => {
+    //     if (themeRef.current) {
+    //         if (themeRef.current.src === lightMode) {
+    //             themeRef.current.src = darkMode;   
+    //         } else {
+    //             themeRef.current.src = lightMode;
+    //         }
+    //     }
+    // }
 
+    const [whatTheme, setTheme] = useState<boolean>(true);
+
+    const changeTheme = () => {
+        setTheme(prevTheme => !prevTheme)
     }
 
     return(
         <>
-        <div className="flex flex-row justify-evenly items-center w-screen h-18 bg-blue-500">
+        <div className="flex flex-row justify-evenly items-center w-screen h-18 bg-blue-800">
             <input className="bg-white rounded-2xl pl-2"/>
             <div className="flex flex-row justify-evenly items-center w-1/4">
-                <label>AMZN</label>
-                <label>AAPL</label>
+                <label className="cursor-pointer text-white hover:text-yellow-500">AMZN</label>
+                <label className="cursor-pointer text-white hover:text-red-500">AAPL</label>
                 <img className="w-10" src={logo}/>
-                <label>MSFT</label>
-                <label>NVDA</label>
+                <label className="cursor-pointer text-white hover:text-orange-500">MSFT</label>
+                <label className="cursor-pointer text-white hover:text-green-500">NVDA</label>
             </div>
 
-            <img className="w-10" src={lightMode} ref={theme} onClick={handleThemeMode}/>
+            <img className="w-10 cursor-pointer" src={whatTheme ? lightMode: darkMode} onClick={() =>changeTheme()}/>
         </div>
         </>
     )
