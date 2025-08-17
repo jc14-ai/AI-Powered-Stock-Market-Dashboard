e137755f7b9231f9e5d608cc97d8bae3bc891d1e
Jzy <jestalycastillo15@gmail.com>
2025-08-17 21:02:44 +0800

    feat: main panel

--- frontend/src/features/MainPanel.tsx
+++ frontend/src/features/MainPanel.tsx
@@ -1,7 +1,16 @@
 export default function MainPanel() {
     return (
         <>
-        
+        <div className="flex flex-col h-screen w-screen bg-blue-200">
+            <div className="flex justify-evenly items-center flex-row h-1/2 w-screen pt-3.5 pb-3.5">
+                <div className="h-full w-2xl bg-white rounded-4xl"></div>
+                <div className="h-full w-2xl bg-white rounded-4xl"></div>
+            </div>
+            <div className="flex justify-evenly items-center flex-row h-1/2 w-screen pt-3.5 pb-3.5">
+                <div className="h-full w-2xl bg-white rounded-4xl"></div>
+                <div className="h-full w-2xl bg-white rounded-4xl"></div>
+            </div>
+        </div>
         </>
     )
 }
