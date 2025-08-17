69159a7bdf334f3d7a140ff1a55be19d836a5e81
Jzy <jestalycastillo15@gmail.com>
2025-08-17 21:47:47 +0800

    Add: timeframe categories

--- frontend/src/features/NavBar.tsx
+++ frontend/src/features/NavBar.tsx
@@ -5,28 +5,39 @@ import logo from "../assets/microsoft.png";
 
 export default function Navbar (){
     // TODO: Implement and fix theme toggle functionality
-    // const themeRef = useRef<HTMLImageElement>(null);
-
-    // const handleThemeMode = ():void => {
-    //     if (themeRef.current) {
-    //         if (themeRef.current.src === lightMode) {
-    //             themeRef.current.src = darkMode;   
-    //         } else {
-    //             themeRef.current.src = lightMode;
-    //         }
-    //     }
-    // }
 
     const [whatTheme, setTheme] = useState<boolean>(true);
+    const [isTimeFrameShown, setTimeFrameVisible] = useState<boolean>(true);
 
     const changeTheme = ():void => {
         setTheme((prevTheme):boolean => !prevTheme)
     }
 
+    const showTimeFrames = ():void => {
+        setTimeFrameVisible((prevBox):boolean => !prevBox)
+        console.log("hovered")
+    }
+
+    // const holdVisible = ():void =>{
+    //     setTimeFrameVisible(():boolean => true)
+    // }
+
     return(
         <>
         <div className="flex flex-row justify-between items-center w-screen h-18 bg-blue-800 pl-10 pr-20">
-            <input className="bg-white rounded-2xl pl-2 h-8"/>
+            <input className="bg-white rounded-2xl pl-2 h-8" 
+            onMouseEnter={():void => showTimeFrames()}
+            onMouseLeave={():void => showTimeFrames()}/>
+
+            <div className={isTimeFrameShown ? "hidden absolute h-20 w-20 bg-white" : "flex absolute h-20 w-20 bg-white"}>
+                <ul className="flex flex-col justify-start items-center m-0">
+                    <li className="hover:bg-blue-100 m-0">1 Day</li>
+                    <li className="hover:bg-blue-100">1 Week</li>
+                    <li className="hover:bg-blue-100">1 Month</li>
+                    <li className="hover:bg-blue-100">1 Year</li>
+                </ul>
+            </div>
+
             <div className="flex flex-row justify-evenly items-center w-1/4">
                 <label className="cursor-pointer text-white hover:text-yellow-500">AMZN</label>
                 <label className="cursor-pointer text-white hover:text-red-500">AAPL</label>
@@ -35,7 +46,7 @@ export default function Navbar (){
                 <label className="cursor-pointer text-white hover:text-green-500">NVDA</label>
             </div>
 
-            <img className="w-10 cursor-pointer" src={whatTheme ? lightMode: darkMode} onClick={():void =>changeTheme()}/>
+            <img className="w-10 cursor-pointer" src={whatTheme ? lightMode : darkMode} onClick={():void =>changeTheme()}/>
         </div>
         </>
     )
