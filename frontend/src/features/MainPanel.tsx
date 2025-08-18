fafbd223e8b5045afe5ed103d4dea07cea2952c5
Jzy <jestalycastillo15@gmail.com>
2025-08-18 16:38:51 +0800

    Add: frame value setter, component type safety

--- frontend/src/features/MainPanel.tsx
+++ frontend/src/features/MainPanel.tsx
@@ -1,16 +1,16 @@
-export default function MainPanel() {
+export default function MainPanel(): React.ReactElement {
     
 
     return (
         <>
-        <div className="flex flex-col h-screen w-screen bg-blue-200">
+        <div className="flex flex-col h-screen w-screen bg-blue-100">
             <div className="flex justify-evenly items-center flex-row h-1/2 w-screen pt-3.5 pb-3.5">
-                <div className="h-full w-2xl bg-white rounded-4xl hover:bg-green-100 cursor-pointer"></div>
-                <div className="h-full w-2xl bg-white rounded-4xl hover:bg-green-100 cursor-pointer"></div>
+                <div className="h-full w-2xl bg-gray-100 rounded-4xl hover:bg-white cursor-pointer"></div>
+                <div className="h-full w-2xl bg-gray-100 rounded-4xl hover:bg-white cursor-pointer"></div>
             </div>
             <div className="flex justify-evenly items-center flex-row h-1/2 w-screen pt-3.5 pb-3.5">
-                <div className="h-full w-2xl bg-white rounded-4xl hover:bg-green-100 cursor-pointer"></div>
-                <div className="h-full w-2xl bg-white rounded-4xl hover:bg-green-100 cursor-pointer"></div>
+                <div className="h-full w-2xl bg-gray-100 rounded-4xl hover:bg-white cursor-pointer"></div>
+                <div className="h-full w-2xl bg-gray-100 rounded-4xl hover:bg-white cursor-pointer"></div>
             </div>
         </div>
         </>
--- frontend/src/features/NavBar.tsx
+++ frontend/src/features/NavBar.tsx
@@ -3,11 +3,12 @@ import darkMode from "../assets/dark_mode.png";
 import lightMode from "../assets/light_mode.png";
 import logo from "../assets/microsoft.png";
 
-export default function Navbar (){
+export default function Navbar (): React.ReactElement{
     // TODO: Implement and fix theme toggle functionality
 
     const [whatTheme, setTheme] = useState<boolean>(true);
     const [isTimeFrameShown, setTimeFrameVisible] = useState<boolean>(true);
+    const [value, setValue] = useState<string>("");
 
     const changeTheme = ():void => {
         setTheme((prevTheme):boolean => !prevTheme)
@@ -25,23 +26,32 @@ export default function Navbar (){
         setTimeFrameVisible(():boolean => true)
     }
 
+    const changeValue = (time:string):void => {
+        setValue(time)
+    }
+
     return(
-        <>
+        <> 
         <div className="flex flex-row justify-between items-center w-screen h-18 bg-blue-800 pl-10 pr-20">
             <div className="">
-                <input className={isTimeFrameShown ? "bg-white rounded-xl pl-3 h-8 w-20 outline-0" : " bg-white pl-3 h-8 w-20 outline-0 rounded-t-xl"}
+                <input className={isTimeFrameShown ? "bg-white rounded-xl pl-2 h-8 w-20 outline-0" : " bg-white pl-2 h-8 w-20 outline-0 rounded-t-xl"}
+                value={value}
                 onMouseEnter={():void => showTimeFrames()}
                 onMouseLeave={():void => showTimeFrames()}/>
 
-                <div className={isTimeFrameShown ? "hidden absolute h-25 w-20 bg-white" : "flex absolute h-25 w-20 bg-white"} 
+                <div className={isTimeFrameShown ? "hidden absolute h-24 w-20 bg-white rounded-b-xl" : "flex absolute h-24 w-20 bg-white rounded-b-xl"} 
                 onMouseEnter={():void => holdVisible()} 
                 onMouseLeave={():void => makeInvisible()}>
 
-                    <ul className="flex flex-col justify-start items-center m-0">
-                        <li className="hover:bg-blue-100 m-0 cursor-pointer">1 Day</li>
-                        <li className="hover:bg-blue-100 cursor-pointer">1 Week</li>
-                        <li className="hover:bg-blue-100 cursor-pointer">1 Month</li>
-                        <li className="hover:bg-blue-100 cursor-pointer">1 Year</li>
+                    <ul className="flex flex-col justify-start items-start m-0 w-full">
+                        <li className="hover:bg-blue-200 cursor-pointer w-full pl-2"
+                        onClick={() => changeValue("1 Day")}>1 Day</li>
+                        <li className="hover:bg-blue-200 cursor-pointer w-full pl-2"
+                        onClick={() => changeValue("1 Week")}>1 Week</li>
+                        <li className="hover:bg-blue-200 cursor-pointer w-full pl-2"
+                        onClick={() => changeValue("1 Month")}>1 Month</li>
+                        <li className="hover:bg-blue-200 cursor-pointer w-full pl-2 rounded-b-xl"
+                        onClick={() => changeValue("1 Year")}>1 Year</li>
                     </ul>
 
                 </div>
