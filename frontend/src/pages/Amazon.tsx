1734916222a6a686d73870a10d15296c77a63b9a
Jzy <jestalycastillo15@gmail.com>
2025-08-23 11:26:16 +0800

    Add: plotly html file

--- frontend/src/pages/Amazon.tsx
+++ frontend/src/pages/Amazon.tsx
@@ -1,9 +1,10 @@
 export default function Amazon():React.ReactElement{
     return (
         <>
-        <div className="flex flex-col h-screen w-screen bg-blue-100">
+        {/* <div className="flex flex-col h-screen w-screen bg-blue-100">
             
-        </div>
+        </div> */}
+        <iframe className="flex flex-col h-screen w-screen bg-blue-100" src="../../AMZN2020.html"/>
         </>
     )
 }
