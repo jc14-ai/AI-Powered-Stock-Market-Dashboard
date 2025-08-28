bf58f2db8481bfd1bf5801fb3be0ff5305a3ff1c
Jzy <jestalycastillo15@gmail.com>
2025-08-28 23:58:49 +0800

    Refactor: full view display components

--- frontend/src/features/MainPanel.tsx
+++ frontend/src/features/MainPanel.tsx
@@ -16,10 +16,9 @@ export default function MainPanel({time}:MainPanelProps): React.ReactElement {
     // const [isFullChart, setFullChart] = useState<boolean>(false);
     const [stockView, setStockView] = useState<stockViewProps>();
 
-    const showFullChart = () => {
+    const showFullChart = (ticker:string, time:string, openPrice:number, closingPrice:number) => {
         // TODO: Fix how to add this in the flow
-        setStockView({ticker:'',time:'', openPrice:0, closingPrice:0})
-        // setFullChart(isFull)
+        setStockView({ticker:ticker,time:time, openPrice:openPrice, closingPrice:closingPrice})
     }
 
     return (
@@ -28,8 +27,13 @@ export default function MainPanel({time}:MainPanelProps): React.ReactElement {
         <div className="flex justify-center items-center flex-col h-[888px] w-screen bg-blue-100">
 
             {/* FULL CHART TOGGLE */}
-            <div className={(stockView?.ticker === 'AMZN' && stockView?.time === 'All Time') ? 'flex justify-center items-center h-150 w-250 bg-gray-800 z-1 absolute rounded-4xl' : (stockView?.ticker === 'AAPL' && stockView?.time === 'All Time' ? '' : '')}>
-                    {/* NOTE: 'hidden h-150 w-250 bg-gray-800 z-1 absolute rounded-4xl' */}
+            <div className={(stockView?.ticker === 'AMZN' || 
+                                stockView?.ticker === 'AAPL' || 
+                                stockView?.ticker === 'MSFT' || 
+                                stockView?.ticker === 'NVDA') ? 
+                                'flex justify-center items-center h-150 w-250 bg-gray-800 z-1 absolute rounded-4xl' : 
+                                'hidden h-150 w-250 bg-gray-800 z-1 absolute rounded-4xl'}>
+                                    
                     {/* Stock Ticker - Company
                         Timeframe selector
                         Line Chart
@@ -46,7 +50,7 @@ export default function MainPanel({time}:MainPanelProps): React.ReactElement {
                 <label className=''>{stockView?.openPrice}</label>
                 <label className=''>{stockView?.closingPrice}</label>
 
-                <button className='bg-amber-50 rounded-xl w-20 cursor-pointer' onClick={():void => showFullChart(false)}>Close</button>
+                <button className='bg-amber-50 rounded-xl w-20 cursor-pointer' onClick={():void => showFullChart('','',0,0)}>Close</button>
             </div>
 
             {/* UPPER MAIN SCREEN */}
@@ -54,7 +58,7 @@ export default function MainPanel({time}:MainPanelProps): React.ReactElement {
                 <div className='h-full flex flex-col items-start
                                 hover:scale-101 duration-300'>
                     <Label text='Amazon.com, Inc.'/>
-                    <div className="h-full w-2xl" onClick={():void => showFullChart(true)}>
+                    <div className="h-full w-2xl" onClick={():void => showFullChart('AMZN',time,0,0)}>
                         <img className='h-full w-full object-cover bg-gray-100 rounded-b-3xl rounded-tr-3xl cursor-pointer' 
                         src={time === "All Time" ? "svg_visuals/AMZN_AT.svg": 
                         (time === "1 Year" ? "svg_visuals/AMZN_Y.svg" : 
@@ -65,7 +69,7 @@ export default function MainPanel({time}:MainPanelProps): React.ReactElement {
                 <div className='h-full flex flex-col justify-between items-start 
                                 hover:scale-101 duration-300'>
                     <Label text='Apple Inc.'/>
-                    <div className="h-full w-2xl" onClick={():void => showFullChart(true)}>
+                    <div className="h-full w-2xl" onClick={():void => showFullChart('AAPL',time,0,0)}>
                         <img className='h-full w-full object-cover bg-gray-100 rounded-b-3xl rounded-tr-3xl cursor-pointer' 
                         src={time === "All Time" ? "svg_visuals/AAPL_AT.svg": 
                         (time === "1 Year" ? "svg_visuals/AAPL_Y.svg" : 
@@ -80,7 +84,7 @@ export default function MainPanel({time}:MainPanelProps): React.ReactElement {
                 <div className='h-full flex flex-col justify-between items-start 
                                 hover:scale-101 duration-300'>
                     <Label text='Microsoft Corporation'/>
-                    <div className="h-full w-2xl" onClick={():void => showFullChart(true)}>
+                    <div className="h-full w-2xl" onClick={():void => showFullChart('MSFT',time,0,0)}>
                         <img className='h-full w-full object-cover bg-gray-100 rounded-b-3xl rounded-tr-3xl cursor-pointer'
                         src={time === "All Time" ? "svg_visuals/MSFT_AT.svg": 
                         (time === "1 Year" ? "svg_visuals/MSFT_Y.svg" : 
@@ -91,7 +95,7 @@ export default function MainPanel({time}:MainPanelProps): React.ReactElement {
                 <div className='h-full flex flex-col justify-between items-start 
                                 hover:scale-101 duration-300'>
                     <Label text='NVIDIA Corporation'/>
-                    <div className="h-full w-2xl" onClick={():void => showFullChart(true)}>
+                    <div className="h-full w-2xl" onClick={():void => showFullChart('NVDA',time,0,0)}>
                         <img className='h-full w-full object-cover bg-gray-100 rounded-b-3xl rounded-tr-3xl cursor-pointer'
                         src={time === "All Time" ? "svg_visuals/NVDA_AT.svg": 
                         (time === "1 Year" ? "svg_visuals/NVDA_Y.svg" : 
