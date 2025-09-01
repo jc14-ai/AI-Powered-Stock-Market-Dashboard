70bc4b63fdd473390b4c4dd4ee9e14ea19c655d3
Jzy <jestalycastillo15@gmail.com>
2025-09-01 23:36:18 +0800

    Add: main routing samples and extra analysis

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,25 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import cors from 'cors';
+import axios from 'axios';
 
 const app = express();
 
 const PORT = 3000;
+
+app.use(cors());
+
+app.get('/hello', (req:Request, res:Response):void => {
+    res.json({'message':'Youre getting this from express!'})
+})
+
+app.get('/predict', async (req:Request, res:Response) => {
+    try {
+        const response = await axios.get('http://localhost:5000/predict');
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({error: 'Flask server error'});
+    }
+})
 
 app.listen(PORT, ():void => {
     console.log(`Listening on PORT ${PORT}`)
